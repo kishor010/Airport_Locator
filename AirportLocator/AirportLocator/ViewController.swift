fd90d0d013973d1726adf6d76ab26e2257af1b5f
Kishor Pahalwani <kishore.pahalwani@impelsys.com>
2020-02-02 20:02:03 +0530

    Initial Commit (Project setup)

---
+++ AirportLocator/AirportLocator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirportLocator
+//
+//  Created by Kishor Pahalwani on 02/02/20.
+//  Copyright © 2020 Kishor Pahalwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
